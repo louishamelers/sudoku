fd441b98a40805cb9025a741a8ea33b18c09ff81
Louis Hamelers <louis@codeowners.nl>
2022-09-25 11:48:17 +0200

    kak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { getSudoku } from 'sudoku-gen';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sudoku-app';
+
+  constructor() {
+    const sudoku = getSudoku('expert');
+    console.log(sudoku);
+  }
 }
