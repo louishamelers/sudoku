0544194bc2f3606ba513d5db019be6a65a896931
Louis Hamelers <louis@codeowners.nl>
2022-09-29 19:17:51 +0200

    mend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameModule } from './modules/game/game.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, GameModule],
   providers: [],
   bootstrap: [AppComponent],
 })
