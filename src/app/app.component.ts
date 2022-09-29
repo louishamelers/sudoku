fbc0c46112e45eef364d1ef82fd759e2df3ff0bd
Louis Hamelers <louis@codeowners.nl>
2022-09-29 19:17:28 +0200

    refactor: clean up app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-import { getSudoku } from 'sudoku-gen';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: '<app-puzzle></app-puzzle>',
 })
 export class AppComponent {
   title = 'sudoku-app';
-
-  constructor() {
-    const sudoku = getSudoku('expert');
-    console.log(sudoku);
-  }
 }
