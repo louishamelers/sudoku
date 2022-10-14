6593d782de245f7458356cf0eca787f79b453734
Louis Hamelers <louis@codeowners.nl>
2022-10-14 17:50:36 +0200

    add @angular/pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ROUTES } from './routes';
 import { CoreModule } from './core/core.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, RouterModule.forRoot(ROUTES)],
+  imports: [CoreModule, RouterModule.forRoot(ROUTES), ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
   bootstrap: [AppComponent],
 })
