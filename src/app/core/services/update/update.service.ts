1f200f888021cb062f6eed56d077250c63cd7693
Louis Hamelers <louis@codeowners.nl>
2022-10-14 22:18:55 +0200

    wip(): add update service

---
+++ src/app/core/services/update/update.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+@Injectable({
+  providedIn: 'root',
+})
+export class UpdateService {
+  constructor(private readonly updates: SwUpdate) {
+    this.updates.available.subscribe((event) => {
+      this.showAppUpdateAlert();
+    });
+  }
+  showAppUpdateAlert() {
+    const header = 'App Update available';
+    const message = 'Choose Ok to update';
+    const action = this.doAppUpdate;
+    const caller = this;
+    // Use MatDialog or ionicframework's AlertController or similar
+    // presentAlert(header, message, action, caller);
+    console.log('there is an update ready!');
+  }
+  doAppUpdate() {
+    this.updates.activateUpdate().then(() => document.location.reload());
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  debugKey: 'game-board',
+  debugKey: '',
   puzzleSize: 9,
   maxErrors: 3,
 };
